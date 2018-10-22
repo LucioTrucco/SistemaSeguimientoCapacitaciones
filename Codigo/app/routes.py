604b4bb7b7ff8155fad1f21ac4deab3a9dcffddd
torsello <matiastorsello@gmail.com>
2018-10-22 14:14:24 -0300

    Adaptando forms y routes
    Se adaptan forms y routes para funcionar con login.html

--- Codigo/app/routes.py
+++ Codigo/app/routes.py
@@ -2,7 +2,6 @@ from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
 
-
 @app.route('/')
 @app.route('/index')
 def index():
@@ -23,7 +22,8 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+        flash('Login requested for user {}'.format(
+            form.username.data))
         return redirect('/index')
-    return render_template('login.html', title='Sign In', form=form)
+    return render_template('login.html', form=form)
+
