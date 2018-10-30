efa84e3806aa15151c8caddc97d47464c7e91036
Marcos Trucco <truccomarcos@gmail.com>
2018-10-30 02:40:09 -0300

    added forms and views to create and assign trainings

--- Codigo/app/forms.py
+++ Codigo/app/forms.py
@@ -1,11 +1,26 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
-from app.models import User
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.fields.html5 import DateField
+
+from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+    remember_me = BooleanField('Remember Me')
+
+
+class TrainingForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    start = DateField('Inicio', format='%Y-%m-%d', validators=[DataRequired()])
+    end = DateField('Fin', format='%Y-%m-%d', validators=[DataRequired()])
+    description = StringField('Descripcion', validators=[DataRequired()])
+
+
+class ClassForm(FlaskForm):
+    date = DateField('Inicio', validators=[DataRequired()])
+    topics = StringField('Name', validators=[DataRequired()])
+    topicsNext = StringField('Name', validators=[DataRequired()])
+    comments = StringField('Name', validators=[DataRequired()])
--- Codigo/app/routes.py
+++ Codigo/app/routes.py
@@ -1,8 +1,10 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
-from app.forms import LoginForm
+from app.forms import LoginForm, TrainingForm
 from flask_login import current_user, login_user, logout_user
 from app.models import User, Training
+from app import db
+
 
 @app.route('/')
 @app.route('/index')
@@ -38,7 +40,16 @@ def logout():
 def trainings():
     return render_template(
         'capacitaciones.html',
-        title='Capcitaciones en Curso')
+        title='Todas las capacitaciones',
+        trainings=Training.query.all())
+
+
+@app.route('/capacitaciones/<string:username>')
+def trainings_user(username):
+    return render_template(
+        'capacitaciones.html',
+        title='Capcitaciones en Curso',
+        trainings=User.query.filter_by(username=username).first().trainings)
 
 
 @app.route('/capacitaciones/<int:id>')
@@ -47,3 +58,50 @@ def details(id):
         'details.html',
         training=Training.query.get(id),
         title='Detalles')
+
+
+@app.route('/capacitaciones/crear', methods=['GET', 'POST'])
+def create():
+    form = TrainingForm()
+    # pdb.set_trace()
+    if form.validate_on_submit():
+        training = Training(
+            name=form.name.data,
+            start=form.start.data,
+            end=form.end.data,
+            finalizada=False,
+            description=form.description.data)
+        db.session.add(training)
+        db.session.commit()
+        return redirect((url_for('details', id=training.id)))
+    return render_template(
+        'capacitacion.html',
+        title='Crear capacitacion',
+        form=form)
+
+
+@app.route('/users')
+def users():
+    return render_template(
+        'users.html',
+        title='Usuarios',
+        users=User.query.all())
+
+
+@app.route('/assign')
+def assign():
+    return render_template(
+        'assign.html',
+        title='Capacitaciones solicitadas',
+        trainings=Training.query.filter_by(trainer=None)
+    )
+
+
+@app.route('/assign/<int:id>')
+def select_trainer(id):
+    return render_template(
+        'select_trainer.html',
+        title='Asignacion',
+        training=Training.query.get(id),
+        available_users=User.query.all()
+    )
